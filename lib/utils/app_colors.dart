c24270902250576bae078dfc00a1d6a07712bd7f
CH MD <39788804+medchek@users.noreply.github.com>
2022-02-12 13:00:30 +0100

    feat: add utility class containing custom colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const appBlue = Color(0xff4D84FF);
+  static const appLightBlue = Color(0xff8AAEFF);
+  static const appDarkBlue = Color(0xff111A47);
+  static const appVividBlue = Color(0xff004FFF);
+  static const appPaleBlue = Color(0xff92CAFC);
+  static const appLightVioletBlue = Color(0xff6282D9);
+  static const appError = Color(0xffE83737);
+  static const appGrey = Color(0xffacacac);
+  static const appLightGrey = Color(0xffF2F5F9);
+  // static const appLightGrey = Color(0xffFBFBFB);
+  static const appLightBlueGrey = Color(0xfff2f5fc);
+  static const appDisabled = Color(0xffC1C9DC);
+  // dark theme colors
+  static const darkAppDarkBlue = Color(0xff0D1437);
+  static const darkAppDisabledBg = Color(0xff2B3154);
+  static const darkAppCardBg = Color(0xff16225A);
+  static const darkAppCanvasBg = Color(0xff01051C);
+
+  static const darkAppDropShadow = Color.fromRGBO(1, 5, 28, 0.7);
+  static const selectDropShadow = Color.fromRGBO(54, 62, 86, 0.16);
+}
