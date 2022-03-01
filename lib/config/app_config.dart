84c0ef4f34d3b94621f690ba8e11dfbaa137bdcf
CH MD <39788804+medchek@users.noreply.github.com>
2022-03-01 19:15:40 +0100

    feat: update app config with production api information

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,14 +1,18 @@
-import 'dart:io' show Platform;
+// import 'dart:io' show Platform;
 
 class AppConfig {
-  static const String apiProtocol = "http";
-  static final String apiHost = Platform.isAndroid
-      ? "10.0.2.2"
-      : "127.0.0.1"; // alias of 127.0.0.1 for AVD
-  // static const String apiHost = "seaal-water-schedule.test";
+  // dev config
+  // static const String apiProtocol = "http";
+  // static final String apiHost = Platform.isAndroid
+  //     ? "10.0.2.2"
+  //     : "127.0.0.1"; // alias of 127.0.0.1 for AVD
+  // static const String apiProtocol = "http";
+  // static const int? apiPort = 8000;
+
+  static const String apiProtocol = "https";
+  static const String apiHost = "water-schedule.herokuapp.com";
   static const String? apiVersion = "v1";
-  static const int? apiPort = 8000;
-  // static const int? apiPort = 443;
+  static const int? apiPort = null;
 
   static const int requestTimeoutSeconds = 10;
 }
