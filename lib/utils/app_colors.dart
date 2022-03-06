cc0c87a79848ce18d055c70a4a3546a65ffd1217
CH MD <39788804+medchek@users.noreply.github.com>
2022-03-06 19:47:56 +0100

    feat: add more custom colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -4,12 +4,14 @@ class AppColors {
   static const appBlue = Color(0xff4D84FF);
   static const appLightBlue = Color(0xff8AAEFF);
   static const appDarkBlue = Color(0xff111A47);
+  static const appDarkBlueVariant = Color(0xff1d2d78);
   static const appVividBlue = Color(0xff004FFF);
   static const appPaleBlue = Color(0xff92CAFC);
   static const appLightVioletBlue = Color(0xff6282D9);
   static const appError = Color(0xffE83737);
   static const appGrey = Color(0xffacacac);
   static const appLightGrey = Color(0xffF2F5F9);
+  static const appGreyVariant = Color(0xffa1a1a1);
   // static const appLightGrey = Color(0xffFBFBFB);
   static const appLightBlueGrey = Color(0xfff2f5fc);
   static const appDisabled = Color(0xffC1C9DC);
